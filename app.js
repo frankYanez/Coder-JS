9895f7bee1fdf46a5065af802f36343c963cd9e4
frank yanez <frankyanez9413@gmail.com>
2022-06-09 18:29:51 -0300

    Agrego estructura del form

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 //Solicito datos para el envio de dinero
 
-let dato = prompt("Ingresa el pais al que deseas enviar dinero");
-alert("Enviaras dinero a " + dato)
+// let dato = prompt("Ingresa el pais al que deseas enviar dinero");
+// alert("Enviaras dinero a " + dato)
 
-let monto = parseInt(prompt("Ingresa el monto"));
+// let monto = parseInt(prompt("Ingresa el monto"));
 
-alert("Enviaras " + monto + " a " + dato)
+// alert("Enviaras " + monto + " a " + dato)
 
-//Interes por envio
-const intereses = 1.5;
+// //Interes por envio
+// const intereses = 1.5;
 
-//Funcion que hace la operacion
-function enviarDinero (n1,n2) {
-    return monto * intereses;
-}
+// //Funcion que hace la operacion
+// function enviarDinero (n1,n2) {
+//     return monto * intereses;
+// }
 
-//Retorno de la operacion
-let resultado = enviarDinero(monto,intereses);
+// //Retorno de la operacion
+// let resultado = enviarDinero(monto,intereses);
 
-function impPais (n1,n2){
-    return resultado + 50;
-}
+// function impPais (n1,n2){
+//     return resultado + 50;
+// }
 
-//Condicional: Si paga impuesto pais o no
+// //Condicional: Si paga impuesto pais o no
 
-if (monto >= 100) {
-    alert("Ademas, debes pagar impuesto pais");
-    alert("El total del envio con recargo es de: $" + resultado);
-}
+// if (monto >= 100) {
+//     alert("Ademas, debes pagar impuesto pais");
+//     alert("El total del envio con recargo es de: $" + resultado);
+// }
 
-else {
-    alert("Por el poco monto que envias, no pagaras impuesto pais");
-}
+// else {
+//     alert("Por el poco monto que envias, no pagaras impuesto pais");
+// }
 
-//Muestro en consola el envio con recargo
+// //Muestro en consola el envio con recargo
 
 
 
